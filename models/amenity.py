c2a4643111d37632297a67a536eda26bdb9ab671
OscarM03 <oscarmutwiri03@gmail.com>
2024-01-12 23:26:24 +0300

    amenities

--- models/amenity.py
+++ models/amenity.py
@@ -5,6 +5,7 @@ Module containing the Amenity class, a subclass of BaseModel.
 
 from models.base_model import BaseModel
 
+
 class Amenity(BaseModel):
     """
     Amenity class represents an amenity that can be
