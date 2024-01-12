3a64e3c019a123abbbe8481745b53dc530969029
OscarM03 <oscarmutwiri03@gmail.com>
2024-01-12 23:32:40 +0300

    file_storage

--- models/engine/file_storage.py
+++ models/engine/file_storage.py
@@ -2,24 +2,38 @@
 import json
 from os.path import exists
 from models.base_model import BaseModel
+from models.user import User
+from models.state import State
+from models.city import City
+from models.amenity import Amenity
+from models.place import Place
+from models.review import Review
+
 
 class FileStorage:
     """
-    A simple file-based storage system for serializing and deserializing objects.
+    A simple file-based storage system for serializing
+    and deserializing objects.
 
     Attributes:
         __file_path (str): Path to the JSON file used for storage.
         __objects (dict): Dictionary to store serialized objects.
 
     Methods:
-        all(self): Returns a dictionary containing all stored objects.
-        new(self, obj): Adds a new object to the storage dictionary.
-        save(self): Serializes and saves the stored objects to the JSON file.
-        reload(self): Deserializes and reloads objects from the JSON file.
+        all(self): Returns a dictionary containing all
+        stored objects.
+        new(self, obj): Adds a new object to the storage
+        dictionary.
+        save(self): Serializes and saves the stored objects
+        to the JSON file.
+        reload(self): Deserializes and reloads objects from
+        the JSON file.
 
     Usage:
-        This class is intended to be used as a singleton for storing and retrieving
-        instances of classes derived from BaseModel. It provides methods to save
+        This class is intended to be used as a singleton
+        for storing and retrieving
+        instances of classes derived from BaseModel. It
+        provides methods to save
         objects to a JSON file and reload them as needed.
     """
 
@@ -49,7 +63,8 @@ class FileStorage:
         """
         Serialize and save the stored objects to the JSON file.
         """
-        serialized_objects = {key: obj.to_dict() for key, obj in FileStorage.__objects.items()}
+        serialized_objects = {key: obj.to_dict() for key,
+                              obj in FileStorage.__objects.items()}
         with open(FileStorage.__file_path, 'w') as file:
             json.dump(serialized_objects, file)
 
@@ -68,4 +83,3 @@ class FileStorage:
                     FileStorage.__objects[key] = instance
         except FileNotFoundError:
             pass
-
