3074465abd6826b01ecd4c026fac892a505c421d
OscarM03 <oscarmutwiri03@gmail.com>
2024-01-12 22:07:44 +0300

    states

---
+++ models/state.py
@@ -0,0 +1,16 @@
+#!/usr/bin/python3
+"""
+Module containing the State class, a subclass of BaseModel.
+"""
+
+from models.base_model import BaseModel
+
+class State(BaseModel):
+    """
+    State class represents a state in the AirBnB application.
+
+    Attributes:
+        name (str): The name of the state.
+    """
+    
+    name = ""
