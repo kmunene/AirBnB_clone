5fa7e75227069a6f8ba98aa81bf0db3ae3a87e77
OscarM03 <oscarmutwiri03@gmail.com>
2024-01-12 22:07:21 +0300

    reviews

---
+++ models/review.py
@@ -0,0 +1,20 @@
+#!/usr/bin/python3
+"""
+Module containing the Review class, a subclass of BaseModel.
+"""
+
+from models.base_model import BaseModel
+
+class Review(BaseModel):
+    """
+    Review class represents a review for a place in the AirBnB application.
+
+    Attributes:
+        place_id (str): The ID of the place for which the review is written.
+        user_id (str): The ID of the user who wrote the review.
+        text (str): The text content of the review.
+    """
+    
+    place_id = ""
+    user_id = ""
+    text = ""
