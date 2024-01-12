9e2fd04f3919db050ce86d933beb9ed46df7a6d9
Oscar Mutwiri <111185564+OscarM03@users.noreply.github.com>
2024-01-12 23:40:46 +0300

    Update __init__.py

--- models/__init__.py
+++ models/__init__.py
@@ -1,3 +1,11 @@
+#!/usr/bin/python3
+"""
+Module: __init__.py
+
+This module initializes the FileStorage instance for the
+AirBnB_clone project.
+
+"""
 from models.engine.file_storage import FileStorage
 
 storage = FileStorage()
