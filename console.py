e722f3bfbf2f5ad057b727f9f6f864751460b483
OscarM03 <oscarmutwiri03@gmail.com>
2024-01-14 11:33:19 +0300

    MyConsole

--- console.py
+++ console.py
@@ -12,7 +12,6 @@ from models.city import City
 from models.amenity import Amenity
 from models.place import Place
 from models.review import Review
-from models import storage
 
 
 class HBNBCommand(cmd.Cmd):
@@ -26,6 +25,123 @@ class HBNBCommand(cmd.Cmd):
     and <class name>.show(<id>) are also supported.
     """
     prompt = "(hbnb) "
+    classes = [BaseModel, User, State, City, Amenity, Place, Review]
+
+    def handle_count(self, class_type):
+        """
+        Handle the <class name>.count() command.
+
+        Parameters:
+            class_type (type): The class type to count instances of.
+
+        Prints:
+            int: The count of instances of the specified class.
+        """
+        count = sum(1 for obj in models.storage.all().values()
+                    if isinstance(obj, class_type))
+        print(count)
+
+    def handle_all(self, class_name):
+        """
+        Handle the <class name>.all() command.
+
+        Parameters:
+            class_name (str): The name of the class to retrieve instances of.
+
+        Prints:
+            str: A list of string representations of instances of
+            the specified class.
+        """
+        all_objs = models.storage.all()
+        if class_name not in (cls.__name__ for cls in self.classes):
+            print("** class doesn't exist **")
+        else:
+            print('[', end="")
+            obj_list = [f'"{all_objs[obj]}"' for obj in all_objs
+                        if obj.startswith(f"{class_name}.")]
+            for index, obj_str in enumerate(obj_list):
+                print(obj_str, end="")
+                if index < len(obj_list) - 1:
+                    print(', ', end="")
+            print(']')
+
+    def handle_show(self, class_name, obj_id):
+        """
+        Handle the <class name>.show(<id>) command.
+
+        Parameters:
+            class_name (str): The name of the class to retrieve an instance of.
+            obj_id (str): The ID of the instance to show.
+
+        Prints:
+            str: The string representation of the specified instance.
+        """
+        key = f"{class_name}.{obj_id}"
+        if key in models.storage.all():
+            print(models.storage.all()[key])
+        else:
+            print("** no instance found **")
+
+    def handle_destroy(self, class_name, obj_id):
+        """
+        Handle the <class name>.destroy(<id>) command.
+
+        Parameters:
+            class_name (str): The name of the class to destroy an instance of.
+            obj_id (str): The ID of the instance to destroy.
+        """
+        key = f"{class_name}.{obj_id}"
+        if key in models.storage.all():
+            del models.storage.all()[key]
+        else:
+            print("** no instance found **")
+
+    def handle_update(self, class_name, obj_id, attr_name, attr_value):
+        """
+        Handle the <class name>.update(<id>, <attribute name>,
+        <attribute value>) command.
+
+        Parameters:
+            class_name (str): The name of the class to update an instance of.
+            obj_id (str): The ID of the instance to update.
+            attr_name (str): The name of the attribute to update.
+            attr_value (str): The new value for the attribute.
+        """
+        key = f"{class_name}.{obj_id}"
+        if key in models.storage.all():
+            obj = models.storage.all()[key]
+            if hasattr(obj, attr_name):
+                setattr(obj, attr_name,
+                        type(getattr(obj, attr_name))(attr_value))
+            else:
+                setattr(obj, attr_name, attr_value)
+            models.storage.save()
+        else:
+            print("** no instance found **")
+
+    def handle_update_dict(self, class_name, obj_id, attr_dict_str):
+        """
+        Handle the <class name>.update(<id>, <dictionary>) command.
+
+        Parameters:
+            class_name (str): The name of the class to update an instance of.
+            obj_id (str): The ID of the instance to update.
+            attr_dict_str (str): The dictionary as a string with
+            attribute names and values.
+        """
+        key = f"{class_name}.{obj_id}"
+        if key in models.storage.all():
+            obj = models.storage.all()[key]
+            attr_dict = eval(attr_dict_str)
+            for attr_name, attr_value in attr_dict.items():
+                if hasattr(obj, attr_name):
+                    setattr(obj, attr_name,
+                            type(getattr(obj, attr_name))(attr_value))
+                else:
+                    setattr(obj, attr_name, attr_value)
+            models.storage.save()
+        else:
+            print("** no instance found **")
 
     def default(self, line):
         """
@@ -36,44 +152,19 @@ class HBNBCommand(cmd.Cmd):
         tokens = line.split('.')
         if len(tokens) == 2:
             class_name, method = tokens
-            classes = [BaseModel, User, State, City, Amenity, Place, Review]
             try:
                 class_type = next(cls for cls in
-                                  classes if cls.__name__ == class_name)
+                                  self.classes if cls.__name__ == class_name)
                 if method == 'count()':
-                    count = sum(1 for obj in storage.all().values()
-                                if isinstance(obj, class_type))
-                    print(count)
-
+                    self.handle_count(class_type)
                 elif method == 'all()':
-                    all_objs = storage.all()
-                    if class_name not in (cls.__name__ for cls in classes):
-                        print("** class doesn't exist **")
-                    else:
-                        print('[', end="")
-                        obj_list = [f'"{all_objs[obj]}"' for obj in all_objs
-                                    if obj.startswith(f"{class_name}.")]
-                        for index, obj_str in enumerate(obj_list):
-                            print(obj_str, end="")
-                            if index < len(obj_list) - 1:
-                                print(', ', end="")
-                        print(']')
+                    self.handle_all(class_name)
                 elif method.startswith('show("') and method.endswith('")'):
                     obj_id = method[len('show("'):-2]
-                    key = f"{class_name}.{obj_id}"
-
-                    if key in models.storage.all():
-                        print(models.storage.all()[key])
-                    else:
-                        print("** no instance found **")
+                    self.handle_show(class_name, obj_id)
                 elif method.startswith('destroy("') and method.endswith('")'):
                     obj_id = method[len('destroy("'):-2]
-                    key = f"{class_name}.{obj_id}"
-
-                    if key in models.storage.all():
-                        del(models.storage.all()[key])
-                    else:
-                        print("** no instance found **")
+                    self.handle_destroy(class_name, obj_id)
                 elif (method.startswith('update("') and
                       method.endswith('")') or method.endswith(')')):
                     if method.endswith('")'):
@@ -87,41 +178,14 @@ class HBNBCommand(cmd.Cmd):
                         match_dict = re.match(r'update\("(.+)",'
                                               r' (\{.*\})', method)
                     if match_attr:
-                        obj_id, atrr_name, atrr_value = match_attr.groups()
-                        key = f"{class_name}.{obj_id}"
-
-                        if key not in storage.all():
-                            print("** no instance found **")
-                            return
-
-                        obj = storage.all()[key]
-                        if hasattr(obj, atrr_name):
-                            setattr(obj, atrr_name,
-                                    type(getattr(obj, atrr_name))
-                                    (atrr_value))
-                            storage.save()
-                        else:
-                            setattr(obj, atrr_name, atrr_value)
-                            storage.save()
+                        obj_id, attr_name, attr_value = match_attr.groups()
+                        self.handle_update(class_name,
+                                           obj_id, attr_name, attr_value)
                     elif match_dict:
                         obj_id, attr_dict_str = match_dict.groups()
                         obj_id = obj_id.replace('"', '')
-                        key = f"{class_name}.{obj_id}"
-
-                        if key not in storage.all():
-                            print("** no instance found **")
-                            return
-
-                        obj = storage.all()[key]
-                        attr_dict = eval(attr_dict_str)
-                        for attr_name, attr_value in attr_dict.items():
-                            if hasattr(obj, attr_name):
-                                setattr(obj, attr_name,
-                                        type(getattr(obj, attr_name))
-                                        (attr_value))
-                            else:
-                                setattr(obj, attr_name, attr_value)
-                        storage.save()
+                        self.handle_update_dict(class_name,
+                                                obj_id, attr_dict_str)
                     else:
                         print("** invalid update format **")
                 else:
@@ -162,7 +226,7 @@ class HBNBCommand(cmd.Cmd):
                 class_type = next(cls for cls in
                                   classes if cls.__name__ == arg)
                 new_instance = class_type()
-                storage.save()
+                models.storage.save()
                 print(new_instance.id)
             except StopIteration:
                 print("** class doesn't exist **")
@@ -229,7 +293,7 @@ class HBNBCommand(cmd.Cmd):
         """
         classes = [BaseModel, User, State, City, Amenity, Place, Review]
 
-        all_objs = storage.all()
+        all_objs = models.storage.all()
         if not arg:
             print('[', end="")
             for index, obj_id in enumerate(all_objs.keys()):
@@ -278,7 +342,7 @@ class HBNBCommand(cmd.Cmd):
 
             key = f"{class_name}.{obj_id}"
 
-            if key not in storage.all():
+            if key not in models.storage.all():
                 print("** no instance found **")
                 return
 
@@ -294,15 +358,15 @@ class HBNBCommand(cmd.Cmd):
 
             attr_value = rest[2]
 
-            obj = storage.all()[key]
+            obj = models.storage.all()[key]
 
             if hasattr(obj, attr_name):
                 setattr(obj, attr_name, type(getattr(obj, attr_name))
                         (attr_value))
-                storage.save()
+                models.storage.save()
             else:
                 setattr(obj, attr_name, attr_value)
-                storage.save()
+                models.storage.save()
 
 
 if __name__ == "__main__":
